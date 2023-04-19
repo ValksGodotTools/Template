33f10d0caba4f2bc7c1b4c1e1f0a22cda1db7c13
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-19 14:02:02 -0500

    Update ResourceOptions.cs

--- Scripts/Resources/ResourceOptions.cs
+++ Scripts/Resources/ResourceOptions.cs
@@ -2,15 +2,18 @@
 
 using VSyncMode = DisplayServer.VSyncMode;
 
-// Note: There is a bug in Godot. If the ResourceOptions.cs script is moved
-// then the file path will not updated in the .tres file. In order to fix
-// this go to C:\Users\VALK-DESKTOP\AppData\Roaming\Godot\app_userdata\Template
-// and delete the .tres file so Godot will be forced to generate it from
-// scratch.
+/* 
+ * If the ResourceOptions.cs script is moved then the file path will not updated
+ * in the .tres file. In order to fix this go to 
+ * C:\Users\VALK-DESKTOP\AppData\Roaming\Godot\app_userdata\Template
+ * and delete the .tres file so Godot will be forced to generate it from
+ * scratch. This is not a Godot bug it is just something to look out for.
+ * 
+ * Resource props must have [Export] attribute otherwise they will not save 
+ * properly.
+ */
 public partial class ResourceOptions : Resource
 {
-    // Resource props must have [Export] attribute otherwise they will not
-    // save properly
     [Export] public float      MusicVolume { get; set; } = 100;
     [Export] public float      SFXVolume   { get; set; } = 100;
     [Export] public WindowMode WindowMode  { get; set; } = WindowMode.Windowed;
