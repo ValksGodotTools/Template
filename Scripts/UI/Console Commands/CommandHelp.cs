159539289a4f8386cf19f2adf454a288f10c7821
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-23 21:40:44 -0500

    Create CommandHelp.cs

---
+++ Scripts/UI/Console Commands/CommandHelp.cs
@@ -0,0 +1,15 @@
+﻿namespace Template;
+
+using System.Reflection;
+
+public class CommandHelp : Command
+{
+    private static List<string> CommandNames { get; } = Assembly.GetExecutingAssembly()
+        .GetTypes()
+        .Where(x => typeof(Command).IsAssignableFrom(x) && !x.IsAbstract)
+        .Select(Activator.CreateInstance).Cast<Command>()
+        .Select(x => x.GetType().Name.Replace("Command", "").ToLower()).ToList();
+
+    public override void Run(string[] args) => 
+        Logger.Log($"Commands:\n{CommandNames.Print()}");
+}
