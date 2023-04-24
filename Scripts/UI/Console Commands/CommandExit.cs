f402a6fdafa28a0aee9baa0fa6e00265c77a30c5
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-23 21:40:49 -0500

    Create CommandExit.cs

---
+++ Scripts/UI/Console Commands/CommandExit.cs
@@ -0,0 +1,11 @@
+﻿namespace Template;
+
+public class CommandExit : Command
+{
+    public CommandExit() => Aliases = new[] { "quit" };
+
+    public override void Run(string[] args)
+    {
+        Global.Quit();
+    }
+}
