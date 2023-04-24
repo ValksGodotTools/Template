052d7b229042d54b5f46da02943652ea77c0ea7a
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-24 13:08:18 -0500

    Create Utils.cs

---
+++ Scripts/2D Platformer/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace Template.Platformer2D;
+
+public static class Utils
+{
+    public static float ClampAndDampen(float horzVelocity, float dampening, float maxSpeedGround)
+    {
+        if (Mathf.Abs(horzVelocity) <= dampening)
+            return 0;
+        else if (horzVelocity > 0)
+            return Mathf.Min(horzVelocity - dampening, maxSpeedGround);
+        else
+            return Mathf.Max(horzVelocity + dampening, -maxSpeedGround);
+    }
+}
