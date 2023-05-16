721583a72a4a511b6918bbaaf06ebd0d066f3f4d
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-15 20:52:29 -0500

    Update Player.cs

--- Scripts/2D Top Down/Player.cs
+++ Scripts/2D Top Down/Player.cs
@@ -5,11 +5,6 @@ public partial class Player : CharacterBody2D
     public float Speed    { get; set; } = 50;
     public float Friction { get; set; } = 0.1f;
 
-    public override void _Ready()
-    {
-        MotionMode = MotionModeEnum.Floating;
-    }
-
     public override void _PhysicsProcess(double delta)
     {
         // Velocity is mutiplied by delta for us already
@@ -21,27 +16,13 @@ public partial class Player : CharacterBody2D
 
     public Vector2 GetMovementInput(string prefix = "")
     {
-        if (!string.IsNullOrWhiteSpace(prefix))
-            prefix += "_";
+        prefix += !string.IsNullOrWhiteSpace(prefix) ? "_" : "";
 
-        // GetActionStrength(...) supports controller sensitivity
-        var inputHorz = Input.GetActionStrength($"{prefix}move_right") - Input.GetActionStrength($"{prefix}move_left");
-        var inputVert = Input.GetActionStrength($"{prefix}move_down") - Input.GetActionStrength($"{prefix}move_up");
-
-        // Normalize vector to prevent fast diagonal strafing
-        return new Vector2(inputHorz, inputVert).Normalized();
+        return Input.GetVector(
+            $"{prefix}move_left", $"{prefix}move_right",
+            $"{prefix}move_up", $"{prefix}move_down");
     }
 
-    public Vector2 GetMovementInputRaw(string prefix = "")
-    {
-        if (!string.IsNullOrWhiteSpace(prefix))
-            prefix += "_";
-
-        // Returns 0, -1 or 1 depending on input
-        var inputHorz = Input.GetActionRawStrength($"{prefix}move_right") - Input.GetActionRawStrength($"{prefix}move_left");
-        var inputVert = Input.GetActionRawStrength($"{prefix}move_down") - Input.GetActionRawStrength($"{prefix}move_up");
-
-        // Normalize vector to prevent fast diagonal strafing
-        return new Vector2(inputHorz, inputVert).Normalized();
-    }
+    public Vector2 GetMovementInputRaw(string prefix = "") => 
+        GetMovementInput(prefix).Round();
 }
