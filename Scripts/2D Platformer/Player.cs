134ff4559d4726aa7e18e9878b74696878e53ede
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-31 11:26:38 -0500

    Remove experimental area code

--- Scripts/2D Platformer/Player.cs
+++ Scripts/2D Platformer/Player.cs
@@ -12,15 +12,6 @@ public partial class Player : CharacterBody2D
     float jumpLossBuildUp;
     bool holdingJumpKey;
 
-    Dictionary<AreaType, (Area2D, AreaData)> areas = new();
-
-    public override void _Ready()
-    {
-        MotionMode = MotionModeEnum.Grounded;
-
-        InitAreas();
-    }
-
     public override void _PhysicsProcess(double delta)
     {
         var horzDir = Input.GetAxis("move_left", "move_right");
@@ -30,8 +21,6 @@ public partial class Player : CharacterBody2D
         vel.X += horzDir * acceleration;
         vel.X = Utils.ClampAndDampen(vel.X, friction, maxSpeed);
 
-        //
-        // //IsColliding(AreaType.Floor)
         if (Input.IsActionJustPressed("jump") && IsOnFloor())
         {
             holdingJumpKey = true;
@@ -56,84 +45,4 @@ public partial class Player : CharacterBody2D
         Velocity = vel;
         MoveAndSlide();
     }
-
-    bool IsColliding(AreaType type) => areas[type].Item2.Detected;
-
-    void InitAreas()
-    {
-        foreach (AreaType type in Enum.GetValues(typeof(AreaType)))
-        {
-            var areaData = new AreaData();
-            areas[type] = (SetupArea(type, areaData), areaData);
-        }
-    }
-
-    Area2D SetupArea(AreaType type, AreaData areaData)
-    {
-        var area = CreateArea(type);
-
-        if (area == null)
-            return null;
-
-        area.BodyEntered += body =>
-        {
-            if (body is TileMap)
-            {
-                areaData.Detected = true;
-                areaData.BodyCount++;
-            }
-        };
-
-        area.BodyExited += body =>
-        {
-            if (body is TileMap)
-            {
-                areaData.BodyCount--;
-                if (areaData.BodyCount == 0)
-                    areaData.Detected = false;
-            }
-        };
-
-        return area;
-    }
-
-    Area2D CreateArea(AreaType type)
-    {
-        var pixelSize = GetNode<Sprite2D>("Sprite2D").GetPixelSize();
-
-        if (type == AreaType.Floor)
-        {
-            var floorHeight = 10;
-
-            var area = new Area2D();
-            var collisionShape = new CollisionShape2D
-            {
-                DebugColor = Colors.Pink,
-                Position = new Vector2(0, pixelSize.Y / 2 + floorHeight / 2),
-                Shape = new RectangleShape2D
-                {
-                    Size = new Vector2(pixelSize.X, floorHeight)
-                }
-            };
-
-            area.AddChild(collisionShape);
-            AddChild(area);
-            return area;
-        }
-
-        return null;
-    }
-}
-
-public class AreaData
-{
-    public bool Detected { get; set; }
-    public int BodyCount { get; set; }
-}
-
-public enum AreaType
-{
-    Floor,
-    WallLeft,
-    WallRight
 }
