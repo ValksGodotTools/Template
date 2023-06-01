345dbb930bb8d1f73e7e4483ebcf53321e4cf799
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-06-01 15:26:54 -0500

    Create PlayerSlide.cs

---
+++ Scripts/2D Platformer/Player/PlayerSlide.cs
@@ -0,0 +1,28 @@
+﻿namespace Template.Platformer2D;
+
+public partial class Player
+{
+    State slide;
+
+    void StateSlide()
+    {
+        slide = new(this, "Slide");
+
+        slide.Enter = () =>
+        {
+
+        };
+
+
+        slide.Update = () =>
+        {
+
+        };
+
+
+        slide.Transitions = () =>
+        {
+
+        };
+    }
+}
