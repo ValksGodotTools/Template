b8f5cc2b0fb1748c1984c29a3ec6f9c0ebd80714
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-06-27 00:56:32 -0500

    Create ModLoader.cs

---
+++ Template/Scripts/Autoloads/ModLoader.cs
@@ -0,0 +1,9 @@
+namespace Template;
+
+public partial class ModLoader : Node
+{
+    public override void _Ready()
+    {
+        // TODO: Load in mods that were enabled from UIModLoader.cs on game startup
+    }
+}
