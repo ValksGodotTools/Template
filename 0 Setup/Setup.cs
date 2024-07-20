e72baa3e64576e9ae50f78dbbcdc9e382a23f402
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-07-20 18:03:10 -0500

    Update Setup.cs

--- 0 Setup/Setup.cs
+++ 0 Setup/Setup.cs
@@ -7,17 +7,11 @@ public partial class Setup : Node
     void _on_apply_changes_pressed()
     {
         string path = ProjectSettings.GlobalizePath("res://");
-        RenameAllNamespaces(path, "Test");
+        RenameAllNamespaces(path, "Banana");
     }
 
     void RenameAllNamespaces(string path, string name)
     {
-        /*string path = ProjectSettings.GlobalizePath("res://0 Setup/Setup.cs");
-        string text = File.ReadAllText(path);
-        text = text.Replace("namespace Template", $"namespace {name}");
-        File.Delete(path);
-        File.WriteAllText(path, text);*/
-        
         using DirAccess dir = DirAccess.Open(path);
 
         if (dir == null)
@@ -32,19 +26,22 @@ public partial class Setup : Node
 
         while (fileName != "")
         {
+            string fullPath = $@"{path}/{fileName}";
+
             if (dir.CurrentIsDir())
             {
                 if (fileName != ".godot" && fileName != "GodotUtils")
                 {
-                    GD.Print($@"{path}/{fileName}");
-                    RenameAllNamespaces($@"{path}/{fileName}", name);
+                    RenameAllNamespaces(fullPath, name);
                 }
             }
             else
             {
                 if (fileName.EndsWith(".cs"))
                 {
-
+                    string text = File.ReadAllText(fullPath);
+                    text = text.Replace("namespace Template", $"namespace {name}");
+                    File.WriteAllText(fullPath, text);
                 }
             }
 
