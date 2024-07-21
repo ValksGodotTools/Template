9e735a64809c3b8157744e61aba15830011f49eb
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-07-21 01:54:20 -0500

    Update Setup.cs

--- 0 Setup/Setup.cs
+++ 0 Setup/Setup.cs
@@ -151,7 +151,7 @@ public partial class Setup : Node
 
     void DeleteDirectoryIfEmpty(string path)
     {
-        if (Directory.GetFiles(path).Length == 0)
+        if (Directory.Exists(path) && Directory.GetFiles(path).Length == 0)
         {
             Directory.Delete(path);
         }
