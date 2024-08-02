1c91476610209c150c53746e79037d65d007f3c8
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-01 19:25:02 -0500

    Move 2D top down util functions to GodotUtils

--- 2D Top Down/Player.cs
+++ 2D Top Down/Player.cs
@@ -1,5 +1,7 @@
 namespace Template.TopDown2D;
 
+using GodotUtils.World2D.TopDown;
+
 public partial class Player : CharacterBody2D
 {
     float speed = 50;
@@ -8,21 +10,9 @@ public partial class Player : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         // Velocity is mutiplied by delta for us already
-        Velocity += GetMovementInput() * speed;
+        Velocity += Utils.GetMovementInput() * speed;
         Velocity = Velocity.Lerp(Vector2.Zero, friction);
 
         MoveAndSlide();
     }
-
-    public Vector2 GetMovementInput(string prefix = "")
-    {
-        prefix += !string.IsNullOrWhiteSpace(prefix) ? "_" : "";
-
-        return Input.GetVector(
-            $"{prefix}move_left", $"{prefix}move_right",
-            $"{prefix}move_up", $"{prefix}move_down");
-    }
-
-    public Vector2 GetMovementInputRaw(string prefix = "") => 
-        GetMovementInput(prefix).Round();
 }
