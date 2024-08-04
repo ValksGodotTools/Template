4b555ec7f31db5325de598fa86d9203cb9eb3a77
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-04 18:16:19 -0500

    Prevent starting server if connected to another

--- Scripts/UI/UINetControlPanel.cs
+++ Scripts/UI/UINetControlPanel.cs
@@ -8,11 +8,34 @@ public partial class UINetControlPanel : Node
     {
         net = new();
 
-        GetNode<Button>("%Start Server").Pressed += net.StartServer;
-        GetNode<Button>("%Stop Server").Pressed += () => net.Server.Stop();
+        Button btnStartServer = GetNode<Button>("%Start Server");
+        Button btnStopServer = GetNode<Button>("%Stop Server");
+
+        btnStartServer.Pressed += net.StartServer;
+        btnStopServer.Pressed += () => net.Server.Stop();
 
         GetNode<Button>("%Start Client").Pressed += net.StartClient;
         GetNode<Button>("%Stop Client").Pressed += net.StopClient;
+
+        net.OnClientCreated += client =>
+        {
+            net.Client.OnConnected += () =>
+            {
+                if (!net.Server.IsRunning)
+                {
+                    // Server is not running and client connected to another server
+                    // Client should not be able to start a server while connected to another server
+                    btnStartServer.Disabled = true;
+                    btnStopServer.Disabled = true;
+                }
+            };
+
+            net.Client.OnDisconnected += opcode =>
+            {
+                btnStartServer.Disabled = false;
+                btnStopServer.Disabled = false;
+            };
+        };
     }
 
     public override void _PhysicsProcess(double delta)
