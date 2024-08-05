28065af0b7e5b5af0ef8b11eb9cc67e84c49fdcf
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-05 12:08:59 -0500

    Client only sends positions if their different

--- 2D Top Down/Scripts/Player.cs
+++ 2D Top Down/Scripts/Player.cs
@@ -4,27 +4,19 @@ using GodotUtils.World2D.TopDown;
 
 public partial class Player : CharacterBody2D
 {
-    GTimer positionEmitInterval;
+    #region Config
 
     float speed = 50;
     float friction = 0.1f;
 
-    public void StartNet()
-    {
-        Net net = Global.Services.Get<Net>();
+    #endregion
 
-        positionEmitInterval = new(this, 
-            milliseconds: GameServer.HeartbeatPosition, looping: true);
+    #region Variables
 
-        positionEmitInterval.Timeout += () =>
-        {
-            net.Client.Send(new CPacketPosition
-            {
-                Position = Position
-            });
-        };
-        positionEmitInterval.Start();
-    }
+    GTimer positionEmitInterval;
+    Vector2 prevPosition;
+
+    #endregion
 
     public override void _PhysicsProcess(double delta)
     {
@@ -33,5 +25,27 @@ public partial class Player : CharacterBody2D
         // Velocity is mutiplied by delta for us already
         Velocity += Utils.GetMovementInput() * speed;
         Velocity = Velocity.Lerp(Vector2.Zero, friction);
+    }
+
+    public void StartNet()
+    {
+        Net net = Global.Services.Get<Net>();
+
+        positionEmitInterval = new(this,
+            milliseconds: GameServer.HeartbeatPosition, looping: true);
+
+        positionEmitInterval.Timeout += () =>
+        {
+            if (Position != prevPosition)
+            {
+                net.Client.Send(new CPacketPosition
+                {
+                    Position = Position
+                });
+            } 
+
+            prevPosition = Position;
+        };
+        positionEmitInterval.Start();
     }
 }
