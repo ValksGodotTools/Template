c0e67f2a7b7090993cf4a9d765223c01bec5a2d1
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-06 21:10:15 -0500

    Fix typo in Game.cs

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -5,7 +5,7 @@ public class Game
     public static SceneManager SceneManager { get => Global.Services.Get<SceneManager>(); }
 
     public static void Log(object message, BBColor color = BBColor.Gray) =>
-        Global.Services.Get<Logger>().Log(message);
+        Global.Services.Get<Logger>().Log(message, color);
 
     public static void LogWarning(object message, BBColor color = BBColor.Orange) =>
         Global.Services.Get<Logger>().LogWarning(message, color);
