8a8b5cd8a92b7883ca014c298134fd2b5b67caab
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-18 18:09:05 -0500

    Move Item script to another file

---
+++ Genres/3D FPS/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace Template.FPS3D;
+
+public class Item // An item the player can hold
+{
+    public Skeleton3D SkeletonRig { get; set; }
+    public AnimationPlayer AnimationPlayer { get; set; }
+
+    Node3D parent;
+
+    public Item(Node3D parent)
+    {
+        this.parent = parent;
+        SkeletonRig = parent.GetNode<Skeleton3D>("Armature/Skeleton3D");
+        AnimationPlayer = parent.GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    public void SetVisible(bool v) => parent.Visible = v;
+}
--- Genres/3D FPS/Scripts/PlayerAnimation.cs
+++ Genres/3D FPS/Scripts/PlayerAnimation.cs
@@ -142,20 +142,3 @@ public partial class Player : CharacterBody3D
         cameraBone.SetUseExternalSkeleton(true);
     }
 }
-
-public class Item // An item the player can hold
-{
-    public Skeleton3D SkeletonRig { get; set; }
-    public AnimationPlayer AnimationPlayer { get; set; }
-
-    Node3D parent;
-
-    public Item(Node3D parent)
-    {
-        this.parent = parent;
-        SkeletonRig = parent.GetNode<Skeleton3D>("Armature/Skeleton3D");
-        AnimationPlayer = parent.GetNode<AnimationPlayer>("AnimationPlayer");
-    }
-
-    public void SetVisible(bool v) => parent.Visible = v;
-}
