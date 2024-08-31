5248473cab4091993ce21d1a6ffce0fd53cfd73a
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-08-31 12:36:11 -0500

    Merge GodotUtils submodule into Template

--- GodotProject/Template/Scripts/Utilities/GMath.cs
+++ GodotProject/Template/Scripts/Utilities/GMath.cs
@@ -37,4 +37,23 @@ public static class GMath
     }
 
     public static float RandAngle() => RandRange(0, Mathf.Pi * 2);
+
+    /// <summary>
+    /// <para>Returns the sum of the first n natural numbers</para>
+    /// <para>For example if n = 4 then this would return 0 + 1 + 2 + 3</para>
+    /// </summary>
+    public static int SumNaturalNumbers(int n) => (n * (n - 1)) / 2;
+
+    public static uint UIntPow(this uint x, uint pow)
+    {
+        uint ret = 1;
+        while (pow != 0)
+        {
+            if ((pow & 1) == 1)
+                ret *= x;
+            x *= x;
+            pow >>= 1;
+        }
+        return ret;
+    }
 }
