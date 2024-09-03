4dd955fb79cb65a330a733110ed5ae7edb1e2145
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-03 10:46:30 -0500

    Resolve #37 by removing light on dash

--- GodotProject/Genres/2D Top Down/Scripts/Player/PlayerDashGhost.cs
+++ GodotProject/Genres/2D Top Down/Scripts/Player/PlayerDashGhost.cs
@@ -5,15 +5,9 @@ public partial class PlayerDashGhost : Node2D
     public override void _Ready()
     {
         const double MODULATE_DURATION = 0.5;
-        const double LIGHT_DURATION = 3.0;
 
         // Animate all canvas items modulate (anything attached to root)
         new GTween(this)
             .Animate(Node2D.PropertyName.Modulate, Colors.Transparent, MODULATE_DURATION).EaseOut();
-
-        // Animate light
-        new GTween(GetNode<PointLight2D>("PointLight2D"))
-            .Animate(PointLight2D.PropertyName.Energy, 0, LIGHT_DURATION).EaseOut()
-            .Callback(() => QueueFree());
     }
 }
