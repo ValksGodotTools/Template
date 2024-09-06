762a7e9bb3301f1524c88ea4d91ca23fbd3414b3
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-06 13:17:23 -0500

    Update ParameterConverter.cs

--- GodotProject/Template/Scripts/UI/Visualize/ParameterConverter.cs
+++ GodotProject/Template/Scripts/UI/Visualize/ParameterConverter.cs
@@ -7,6 +7,20 @@ public static class ParameterConverter
 {
     public static object[] ConvertParameterInfoToObjectArray(ParameterInfo[] paramInfos, object[] providedValues)
     {
+        ValidateInput(paramInfos, providedValues);
+
+        object[] parameters = new object[paramInfos.Length];
+
+        for (int i = 0; i < paramInfos.Length; i++)
+        {
+            parameters[i] = ConvertParameter(paramInfos[i], providedValues[i]);
+        }
+
+        return parameters;
+    }
+
+    private static void ValidateInput(ParameterInfo[] paramInfos, object[] providedValues)
+    {
         if (paramInfos == null)
         {
             throw new ArgumentNullException(nameof(paramInfos));
@@ -21,41 +35,35 @@ public static class ParameterConverter
         {
             throw new ArgumentException("The number of provided values does not match the number of method parameters.");
         }
+    }
 
-        object[] parameters = new object[paramInfos.Length];
-
-        for (int i = 0; i < paramInfos.Length; i++)
+    private static object ConvertParameter(ParameterInfo paramInfo, object providedValue)
+    {
+        if (providedValue == null)
         {
-            ParameterInfo paramInfo = paramInfos[i];
-            object providedValue = providedValues[i];
-
-            if (providedValue == null)
-            {
-                if (paramInfo.ParameterType == typeof(string))
-                {
-                    parameters[i] = null;
-                }
-                else if (paramInfo.ParameterType.IsValueType)
-                {
-                    // Assign default value for value types
-                    parameters[i] = Activator.CreateInstance(paramInfo.ParameterType);
-                }
-                else
-                {
-                    parameters[i] = null;
-                }
-            }
-            else
-            {
-                if (!paramInfo.ParameterType.IsAssignableFrom(providedValue.GetType()))
-                {
-                    throw new InvalidOperationException($"The provided value for parameter '{paramInfo.Name}' is not assignable to the parameter type '{paramInfo.ParameterType}'.");
-                }
-
-                parameters[i] = providedValue;
-            }
+            return GetDefaultValue(paramInfo.ParameterType);
         }
 
-        return parameters;
+        if (!paramInfo.ParameterType.IsAssignableFrom(providedValue.GetType()))
+        {
+            throw new InvalidOperationException($"The provided value for parameter '{paramInfo.Name}' is not assignable to the parameter type '{paramInfo.ParameterType}'.");
+        }
+
+        return providedValue;
+    }
+
+    private static object GetDefaultValue(Type type)
+    {
+        if (type == typeof(string))
+        {
+            return null;
+        }
+
+        if (type.IsValueType)
+        {
+            return Activator.CreateInstance(type);
+        }
+
+        return null;
     }
 }
