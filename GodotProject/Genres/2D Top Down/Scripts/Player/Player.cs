58f75123762e3cc36c7edfe0e490ace1d00a6dca
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-09 17:47:01 -0500

    Add support for controllers to top down player

--- GodotProject/Genres/2D Top Down/Scripts/Player/Player.cs
+++ GodotProject/Genres/2D Top Down/Scripts/Player/Player.cs
@@ -17,9 +17,11 @@ public partial class Player : Character, INetPlayer
 
     CameraShakeComponent cameraShake;
     Vector2 prevPosition;
-    Vector2 direction;
+    Vector2 moveDirection;
     GameClient client;
     Sprite2D sprite;
+    Sprite2D cursor;
+    double controllerLookInputsActiveBuffer;
 
     bool canDash;
 
@@ -31,23 +33,77 @@ public partial class Player : Character, INetPlayer
         canDash = true;
         client = Game.Net.Client;
         sprite = GetNode<Sprite2D>("Sprite2D");
+        cursor = GetNode<Sprite2D>("Cursor");
         cameraShake = GetTree().Root.GetNode<CameraShakeComponent>("Level/Camera2D/CameraShake");
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
-        direction = GodotUtils.World2D.TopDown.Utils.GetMovementInput();
+
+        Vector2 moveDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        
+        moveDirection += new Vector2(Input.GetJoyAxis(0, JoyAxis.LeftX), Input.GetJoyAxis(0, JoyAxis.LeftY));
+        
+        moveDirection = moveDirection.Normalized();
+
+        this.moveDirection = moveDirection;
 
         // Velocity is mutiplied by delta for us already
-        Velocity += direction * speed;
+        Velocity += moveDirection * speed;
         Velocity = Velocity.Lerp(Vector2.Zero, friction);
 
-        if (Input.IsActionJustPressed("dash") && canDash && direction != Vector2.Zero)
+        if (Input.IsActionJustPressed("dash") && canDash && moveDirection != Vector2.Zero)
         {
             Dash();
             canDash = false;
             GTween.Delay(this, 0.2, () => canDash = true);
+        }
+
+        Vector2 lookDirection = Vector2.Zero;
+
+        if (controllerLookInputsActiveBuffer > 0)
+        {
+            lookDirection = Input.GetJoyName(0) switch
+            {
+                // My PS2 controller shows up as "PS3 Controller" when I do Input.GetJoyName(0)
+                // My PS2 controller requires a specific implementation in order for it to work in Godot
+                "PS3 Controller" => new Vector2(Input.GetJoyAxis(0, JoyAxis.RightX), -(Input.GetJoyAxis(0, JoyAxis.TriggerLeft) - 0.5f) * 2),
+
+                // All other controllers
+                _ => new Vector2(Input.GetJoyAxis(0, JoyAxis.RightX), Input.GetJoyAxis(0, JoyAxis.RightY))
+            };
+        }
+        else
+        {
+            lookDirection = GetGlobalMousePosition() - Position;
+        }
+
+        cursor.LookAt(Position + lookDirection.Normalized() * 100);
+
+        AreControllerLookInputsActive();
+        controllerLookInputsActiveBuffer -= delta;
+    }
+
+    private void AreControllerLookInputsActive()
+    {
+        float rightX = Input.GetJoyAxis(0, JoyAxis.RightX);
+        float rightY = Input.GetJoyAxis(0, JoyAxis.RightY);
+
+        float triggerLeft = 0;
+        
+        if (Input.GetJoyName(0) == "PS3 Controller")
+        {
+            triggerLeft = -(Input.GetJoyAxis(0, JoyAxis.TriggerLeft) - 0.5f) * 2;
+        }
+
+        float sum = Mathf.Abs(rightX) + Mathf.Abs(rightY) + Mathf.Abs(triggerLeft);
+
+        GD.Print(sum);
+
+        if (sum > 0.3)
+        {
+            controllerLookInputsActiveBuffer = 1;
         }
     }
 
@@ -59,7 +115,7 @@ public partial class Player : Character, INetPlayer
             .Loop();
 
         new GTween(this)
-            .Animate(CharacterBody2D.PropertyName.Velocity, direction * dashStrength, 0.1)
+            .Animate(CharacterBody2D.PropertyName.Velocity, moveDirection * dashStrength, 0.1)
             .Delay(0.1)
             .Callback(() => ghosts.Stop());
     }
