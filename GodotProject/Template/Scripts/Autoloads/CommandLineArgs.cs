d91598a193505a4de8af2f3b7d66e89d5fcd1d48
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-20 03:10:50 -0500

    Make CommandLineArgs.cs more presentable

--- GodotProject/Template/Scripts/Autoloads/CommandLineArgs.cs
+++ GodotProject/Template/Scripts/Autoloads/CommandLineArgs.cs
@@ -1,81 +1,99 @@
 using Godot;
+using System.Collections.Generic;
 
 namespace Template;
 
+/// <summary>
+/// Handles custom command line arguments set for each instance.
+/// </summary>
 public partial class CommandLineArgs : Node
 {
     public override void _Ready()
     {
-        // Get the command-line arguments
+        // Get command-line arguments
         string[] args = OS.GetCmdlineArgs();
 
-        // Initialize the window position and size
-        Vector2I windowPosition = Vector2I.Zero;
-        Vector2I windowSize = Vector2I.Zero;
+        Dictionary<string, WindowSettings> windowSettings = GetWindowSettingsMap();
 
-        // Get the screen size
-        Vector2 screenSize = DisplayServer.ScreenGetSize();
-
-        // Define the vertical space for the window bar
-        int windowBarHeight = 30; // Adjust this value based on your actual window bar height
-
-        // Loop through the arguments to find the position argument
+        // Loop through arguments to find the position argument
         foreach (string arg in args)
         {
-            if (arg == "top_left")
+            if (windowSettings.TryGetValue(arg, out WindowSettings settings))
             {
-                windowPosition = new Vector2I(0, windowBarHeight);
-                windowSize = new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2));
-                break;
-            }
-            else if (arg == "top_right")
-            {
-                windowPosition = new Vector2I((int)(screenSize.X / 2), windowBarHeight);
-                windowSize = new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2));
-                break;
-            }
-            else if (arg == "bottom_left")
-            {
-                windowPosition = new Vector2I(0, (int)(screenSize.Y / 2) + windowBarHeight);
-                windowSize = new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2));
-                break;
-            }
-            else if (arg == "bottom_right")
-            {
-                windowPosition = new Vector2I((int)(screenSize.X / 2), (int)(screenSize.Y / 2) + windowBarHeight);
-                windowSize = new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2));
-                break;
-            }
-            else if (arg == "middle_left")
-            {
-                windowPosition = new Vector2I(0, windowBarHeight);
-                windowSize = new Vector2I((int)(screenSize.X / 2), (int)(screenSize.Y - windowBarHeight));
-                break;
-            }
-            else if (arg == "middle_right")
-            {
-                windowPosition = new Vector2I((int)(screenSize.X / 2), windowBarHeight);
-                windowSize = new Vector2I((int)(screenSize.X / 2), (int)(screenSize.Y - windowBarHeight));
-                break;
-            }
-            else if (arg == "middle_top")
-            {
-                windowPosition = new Vector2I(0, windowBarHeight);
-                windowSize = new Vector2I((int)screenSize.X, (int)((screenSize.Y - windowBarHeight) / 2));
-                break;
-            }
-            else if (arg == "middle_bottom")
-            {
-                windowPosition = new Vector2I(0, (int)(screenSize.Y / 2) + windowBarHeight);
-                windowSize = new Vector2I((int)screenSize.X, (int)((screenSize.Y - windowBarHeight) / 2));
+                // Set the window size
+                DisplayServer.WindowSetSize(settings.Size);
+
+                // Set the window position
+                DisplayServer.WindowSetPosition(settings.Position);
                 break;
             }
         }
+    }
 
-        // Set the window size
-        DisplayServer.WindowSetSize(windowSize);
+    private static Dictionary<string, WindowSettings> GetWindowSettingsMap()
+    {
+        // Define the vertical space for the window bar
+        int windowBarHeight = 30; // Adjust this value based on your actual window bar height
 
-        // Set the window position
-        DisplayServer.WindowSetPosition(windowPosition);
+        // Get screen size
+        Vector2 screenSize = DisplayServer.ScreenGetSize();
+
+        return new Dictionary<string, WindowSettings>()
+        {
+            {
+                "top_left",
+                new WindowSettings(
+                    new Vector2I(0, windowBarHeight),
+                    new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2)))
+            },
+            {
+                "top_right",
+                new WindowSettings(
+                    new Vector2I((int)(screenSize.X / 2), windowBarHeight),
+                    new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2)))
+            },
+            {
+                "bottom_left",
+                new WindowSettings(
+                    new Vector2I(0, (int)(screenSize.Y / 2) + windowBarHeight),
+                    new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2)))
+            },
+            {
+                "bottom_right",
+                new WindowSettings(
+                    new Vector2I((int)(screenSize.X / 2), (int)(screenSize.Y / 2) + windowBarHeight),
+                    new Vector2I((int)(screenSize.X / 2), (int)((screenSize.Y - windowBarHeight) / 2)))
+            },
+            {
+                "middle_left",
+                new WindowSettings(
+                    new Vector2I(0, windowBarHeight),
+                    new Vector2I((int)(screenSize.X / 2), (int)(screenSize.Y - windowBarHeight)))
+            },
+            {
+                "middle_right",
+                new WindowSettings(
+                    new Vector2I((int)(screenSize.X / 2), windowBarHeight),
+                    new Vector2I((int)(screenSize.X / 2), (int)(screenSize.Y - windowBarHeight)))
+            },
+            {
+                "middle_top",
+                new WindowSettings(
+                    new Vector2I(0, windowBarHeight),
+                    new Vector2I((int)screenSize.X, (int)((screenSize.Y - windowBarHeight) / 2)))
+            },
+            {
+                "middle_bottom",
+                new WindowSettings(
+                    new Vector2I(0, (int)(screenSize.Y / 2) + windowBarHeight),
+                    new Vector2I((int)screenSize.X, (int)((screenSize.Y - windowBarHeight) / 2)))
+            }
+        };
+    }
+
+    private class WindowSettings(Vector2I position, Vector2I size)
+    {
+        public Vector2I Position { get; } = position;
+        public Vector2I Size { get; } = size;
     }
 }
