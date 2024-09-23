6cdb53a1816fc8e6a4f683e85fdebc336ce28dd1
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-23 14:27:33 -0500

    Add item count label to each inventory item

--- GodotProject/Sandbox/Inventory/Scripts/DraggableItem.cs
+++ GodotProject/Sandbox/Inventory/Scripts/DraggableItem.cs
@@ -1,11 +1,25 @@
 ﻿using Godot;
 using GodotUtils;
+using System.Drawing;
 
 namespace Template.Inventory;
 
 [Draggable]
 public partial class DraggableItem : AnimatedSprite2D, IDraggable
 {
+    private Label _itemCountLabel;
+
+    public override void _Ready()
+    {
+        _itemCountLabel = CreateItemCountLabel();
+        AddChild(_itemCountLabel);
+    }
+
+    public void SetItemCount(int count)
+    {
+        _itemCountLabel.Text = count.ToString();
+    }
+
     public void OnDragReleased()
     {
         Area2D area = CursorUtils2D.GetAreaUnder(this);
@@ -13,5 +27,23 @@ public partial class DraggableItem : AnimatedSprite2D, IDraggable
         DraggableItem otherItem = area?.GetParent<DraggableItem>();
 
         GD.Print(otherItem);
+    }
+
+    private Label CreateItemCountLabel()
+    {
+        Vector2 size = this.GetSize();
+
+        Label label = new()
+        {
+            Text = "0",
+            Scale = Vector2.One * 0.25f,
+            Position = size * 0.1f + new Vector2(size.X * 0.3f, 0),
+            LabelSettings = new LabelSettings
+            {
+                FontSize = 32,
+            },
+        };
+
+        return label;
     }
 }
