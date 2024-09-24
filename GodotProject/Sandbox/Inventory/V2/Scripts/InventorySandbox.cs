7fae414ce6d23047dd470a8d3704f39c93754ce0
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-24 14:15:34 -0500

    Cleanup UIInventory.cs

--- GodotProject/Sandbox/Inventory/V2/Scripts/InventorySandbox.cs
+++ GodotProject/Sandbox/Inventory/V2/Scripts/InventorySandbox.cs
@@ -14,31 +14,3 @@ public partial class InventorySandbox : Node
         _ = new UIInventory(inventory, this);
     }
 }
-
-public class UIInventory
-{
-    public UIInventory(Inventory inventory, Node parent)
-    {
-        AddItemContainers(inventory, parent);
-    }
-
-    private void AddItemContainers(Inventory inventory, Node parent)
-    {
-        InventoryContainer inventoryContainer = InventoryContainer.Instantiate();
-
-        parent.AddChild(inventoryContainer);
-
-        for (int i = 0; i < inventory.GetInventorySize(); i++)
-        {
-            ItemContainer itemContainer = inventoryContainer.AddItemContainer();
-
-            Item item = inventory.GetItem(i);
-
-            if (item != null)
-            {
-                itemContainer.SetSpriteFrames(item.ResourcePath);
-                itemContainer.SetCount(item.Count);
-            }
-        }
-    }
-}
---
+++ GodotProject/Sandbox/Inventory/V2/Scripts/UI/UIInventory.cs
@@ -0,0 +1,40 @@
+﻿using Godot;
+
+namespace Template.InventoryV2;
+
+public class UIInventory
+{
+    public UIInventory(Inventory inventory, Node parent)
+    {
+        // Create the InventoryContainer
+        InventoryContainer invContainer = InventoryContainer.Instantiate();
+
+        // Add InventoryContainer to the tree as soon as possible so _Ready() functions in
+        // children nodes will get called at the correct times
+        parent.AddChild(invContainer);
+
+        // Add the item containers
+        AddItemContainers(invContainer, inventory);
+    }
+
+    private void AddItemContainers(InventoryContainer invContainer, Inventory inv)
+    {
+        for (int i = 0; i < inv.GetInventorySize(); i++)
+        {
+            AddItemContainer(invContainer, inv, i);
+        }
+    }
+
+    private void AddItemContainer(InventoryContainer invContainer, Inventory inv, int index)
+    {
+        ItemContainer itemContainer = invContainer.AddItemContainer();
+
+        Item item = inv.GetItem(index);
+
+        if (item != null)
+        {
+            itemContainer.SetSpriteFrames(item.ResourcePath);
+            itemContainer.SetCount(item.Count);
+        }
+    }
+}
