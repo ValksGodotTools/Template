8bc3ea0848f977782244bb3ac70ec6e89711c643
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-25 18:53:34 -0500

    Add XML summaries to InventoryInputHandler

--- GodotProject/Sandbox/Inventory/Scripts/UI/Utils/InventoryInputHandler.cs
+++ GodotProject/Sandbox/Inventory/Scripts/UI/Utils/InventoryInputHandler.cs
@@ -37,9 +37,18 @@ public abstract class InventoryInputHandler
         }
     }
 
+    /// <summary>If the correct <paramref name="mouseBtn"/> input is provided then handle this input.</summary>
     public abstract bool CheckInput(InputEventMouseButton mouseBtn);
+    
+    /// <summary>Both items are of the same type. The cursor and inventory slots are both guaranteed to have at least one item.</summary>
     public abstract void HandleSameType(InventorySlotContext context);
+    
+    /// <summary>Both items are of different types. The cursor and inventory slots are both guaranteed to have at least one item.</summary>
     public abstract void HandleDiffType(InventorySlotContext context);
+    
+    /// <summary>Placing an item from the cursor to the inventory. The cursor is guaranteed to have at least one item and there is no item in the inventory slot.</summary>
     public abstract void HandlePlace(InventorySlotContext context);
+    
+    /// <summary>Picking up an item from the inventory to the cursor. The inventory slot is guaranteed to have at least one item and there is no item in the cursor slot.</summary>
     public abstract void HandlePickup(InventorySlotContext context);
 }
