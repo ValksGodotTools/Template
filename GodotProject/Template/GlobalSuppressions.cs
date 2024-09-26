0027be641543b7fcb7f82139eaefda12523d6d20
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-26 16:34:54 -0500

    Delete GlobalSuppressions.cs

--- GodotProject/Template/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "No idea how to do this", Scope = "member", Target = "~M:CSharpUtils.ExtensionsString.IsAddress(System.String)~System.Boolean")]
-[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "No idea how to do this", Scope = "member", Target = "~M:GodotUtils.SceneFileUtils.FixBrokenResourcePaths(System.String)")]
