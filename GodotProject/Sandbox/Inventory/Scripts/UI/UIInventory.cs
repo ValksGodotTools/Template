9beab5abdf030944ada3bade4e4c8c96465808c9
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-26 19:44:45 -0500

    Revert abstraction and redo logic from scratch

--- GodotProject/Sandbox/Inventory/Scripts/UI/UIInventory.cs
+++ GodotProject/Sandbox/Inventory/Scripts/UI/UIInventory.cs
@@ -1,4 +1,5 @@
 ﻿using Godot;
+using GodotUtils;
 
 namespace Template.Inventory;
 
@@ -22,60 +23,198 @@ public class UIInventory
         int invSize = inv.GetInventorySize();
         ItemContainer[] itemContainers = new ItemContainer[invSize];
 
-        InventoryInputHandler[] inputs = 
-        [
-            new InventoryInputLeftClick(),
-            new InventoryInputRightClick()
-        ];
-
         for (int i = 0; i < invSize; i++)
         {
             CursorItemContainer cursorItemContainer = ServiceProvider.Services.Get<CursorItemContainer>();
             InventorySlotContext context = new(cursorItemContainer, inv, invContainer.AddItemContainer(), i);
             
-            AddItemContainer(inputs, i, itemContainers, context);
+            AddItemContainer(i, itemContainers, context);
         }
 
-        UpdateItemContainerOnInvChanged(itemContainers, inv);
+        inv.OnItemChanged += (index, item) =>
+        {
+            itemContainers[index].SetItem(item);
+        };
     }
 
-    private void AddItemContainer(InventoryInputHandler[] inputs, int index, ItemContainer[] itemContainers, InventorySlotContext context)
+    private void AddItemContainer(int index, ItemContainer[] itemContainers, InventorySlotContext context)
     {
         ItemContainer itemContainer = context.ItemContainer;
         itemContainer.SetItem(context.Inventory.GetItem(context.Index));
 
-        itemContainer.MouseEntered += () =>
-        {
-            // TODO: Will be used for checking if an item was dropped in the world
-        };
-
-        itemContainer.MouseExited += () =>
-        {
-            // TODO: Will be used for checking if an item was dropped in the world
-        };
+        Inventory inv = context.Inventory;
+        CursorInventory cInv = context.CursorInventory;
+        InventoryManager invM = context.InventoryManager;
+        CursorManager cM = context.CursorManager;
 
         itemContainer.GuiInput += inputEvent =>
         {
             if (inputEvent is InputEventMouseButton mouseBtn)
             {
-                foreach (InventoryInputHandler input in inputs)
+                if (mouseBtn.IsLeftClickJustPressed())
                 {
-                    if (input.HasInput(mouseBtn))
+                    // Cursor has item
+                    if (cInv.HasItem())
                     {
-                        input.Handle(context);
+                        // Cursor and inventory have items
+                        if (inv.HasItem(index))
+                        {
+                            if (cInv.GetItem().Equals(inv.GetItem(index)))
+                            {
+                                // Stack the cursor item onto the inventory item
+                                cM.GetItemAndFrame(out Item cItem, out int cFrame);
+
+                                cItem.AddCount(inv.GetItem(index).Count);
+
+                                invM.SetItemAndFrame(cItem, cFrame);
+
+                                cInv.ClearItem();
+                            }
+                            else
+                            {
+                                // Swap cursor item with inventory item
+                                invM.GetItemAndFrame(out Item item, out int frame);
+                                cM.GetItemAndFrame(out Item cItem, out int cFrame);
+
+                                invM.SetItemAndFrame(cItem, cFrame);
+                                cM.SetItemAndFrame(item, frame);
+                                cM.SetPosition(itemContainer.GlobalPosition);
+                            }
+                        }
+                        // Cursor has item but inventory does not
+                        else
+                        {
+                            // Place cursor item
+                            cM.GetItemAndFrame(out Item cItem, out int cFrame);
+                            invM.SetItemAndFrame(cItem, cFrame);
+
+                            cInv.ClearItem();
+                        }
+                    }
+                    // Cursor has no item
+                    else
+                    {
+                        // Cursor has no item but inventory item exists
+                        if (inv.HasItem(index))
+                        {
+                            // Pickup inventory item
+                            invM.GetItemAndFrame(out Item item, out int frame);
+
+                            cM.SetItemAndFrame(item, frame);
+                            cM.SetPosition(itemContainer.GlobalPosition);
+
+                            inv.ClearItem(index);
+                        }
+                    }
+                }
+                else if (mouseBtn.IsRightClickJustPressed())
+                {
+                    // Cursor has item
+                    if (cInv.HasItem())
+                    {
+                        // Cursor and inventory have items
+                        if (inv.HasItem(index))
+                        {
+                            if (cInv.GetItem().Equals(inv.GetItem(index)))
+                            {
+                                // Stack one of the cursor item onto the inventory item
+
+                                // Add one to item
+                                invM.GetItemAndFrame(out Item item, out int frame);
+                                item.AddCount(1);
+                                invM.SetItemAndFrame(item, frame);
+
+                                // Remove one from cursor item
+                                Item cItem = cInv.GetItem();
+                                cItem.RemoveCount(1);
+
+                                // Update cursor
+                                if (cItem.Count <= 0)
+                                {
+                                    cInv.ClearItem();
+                                }
+                                else
+                                {
+                                    cM.GetItemAndFrame(out Item _, out int cFrame);
+                                    cM.SetItemAndFrame(cItem, cFrame);
+                                }
+                            }
+                            else
+                            {
+                                // Swap cursor item with inventory item
+                                invM.GetItemAndFrame(out Item item, out int frame);
+                                cM.GetItemAndFrame(out Item cItem, out int cFrame);
+
+                                invM.SetItemAndFrame(cItem, cFrame);
+                                cM.SetItemAndFrame(item, frame);
+                                cM.SetPosition(itemContainer.GlobalPosition);
+                            }
+                        }
+                        // Cursor has item but inventory does not
+                        else
+                        {
+                            // Place one of cursor item
+                            cM.GetItemAndFrame(out Item cItem, out int cFrame);
+                            
+                            Item nItem = new(cItem);
+                            nItem.SetCount(1);
+
+                            invM.SetItemAndFrame(nItem, cFrame);
+
+                            // Update cursor
+                            cItem.RemoveCount(1);
+
+                            if (cItem.Count <= 0)
+                            {
+                                cInv.ClearItem();
+                            }
+                            else
+                            {
+                                //cM.GetItemAndFrame(out Item _, out int _);
+                                cM.SetItemAndFrame(cItem, cFrame);
+                            }
+                        }
+                    }
+                    // Cursor has no item
+                    else
+                    {
+                        // Cursor has no item but inventory item exists
+                        if (inv.HasItem(index))
+                        {
+                            // Pickup half of the inventory item
+                            invM.GetItemAndFrame(out Item item, out int frame);
+
+                            Item nItem = new(item);
+
+                            int halfCount = item.Count / 2;
+
+                            // Prevent halfCount from equaling zero
+                            if (item.Count == 1)
+                            {
+                                halfCount = 1;
+                            }
+
+                            nItem.SetCount(halfCount);
+
+                            item.RemoveCount(halfCount);
+
+                            if (item.Count <= 0)
+                            {
+                                inv.ClearItem(index);
+                            }
+                            else
+                            {
+                                invM.SetItemAndFrame(item, frame);
+                            }
+
+                            cM.SetItemAndFrame(nItem, frame);
+                            cM.SetPosition(itemContainer.GlobalPosition);
+                        }
                     }
                 }
             }
         };
 
         itemContainers[index] = itemContainer;
-    }
-
-    private void UpdateItemContainerOnInvChanged(ItemContainer[] itemContainers, Inventory inv)
-    {
-        inv.OnItemChanged += (index, item) =>
-        {
-            itemContainers[index].SetItem(item);
-        };
     }
 }
