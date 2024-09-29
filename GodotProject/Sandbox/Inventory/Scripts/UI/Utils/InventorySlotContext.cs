d6952294529fda644fa5b936d6f937a4fabe6c71
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-29 18:09:39 -0500

    Delete InventorySlotContext.cs

--- GodotProject/Sandbox/Inventory/Scripts/UI/Utils/InventorySlotContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Template.Inventory;
-
-public class InventorySlotContext(CursorItemContainer cursorItemContainer, Inventory inv, ItemContainer itemContainer, int index)
-{
-    public Inventory CursorInventory { get; } = cursorItemContainer.Inventory;
-    public Inventory Inventory { get; } = inv;
-    public ItemContainer ItemContainer { get; } = itemContainer;
-    public int Index { get; } = index;
-}
