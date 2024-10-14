5800a6038c9b27ae29774aca9c2122aa4266b73d
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-10-14 18:04:47 -0500

    You can actually fly now

--- Genres/3D FPS/Scenes/SpaceShip.cs
+++ Genres/3D FPS/Scenes/SpaceShip.cs
@@ -7,19 +7,43 @@ namespace Template.FPS3D;
 public partial class SpaceShip : Node3D, IStateMachine
 {
     private State _curState;
-    private CharacterBody3D _body;
-    private Vector3 _velocity;
-    
+    private Quaternion _quatYawPitch;
+    private float _yaw;
+    private float _pitch;
+    private bool _isFlying;
+    private Vector3 _updatedPosition;
+
     public override void _Ready()
     {
         _curState = Dorment();
-        _body = _.CharacterBody3D;
+        _quatYawPitch = Quaternion;
+        _updatedPosition = Position;
         TakeControlOfShip();
     }
-
+    
     public override void _PhysicsProcess(double delta)
     {
         _curState.Update((float)delta);
+
+        Quaternion = _quatYawPitch;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (_isFlying && @event is InputEventMouseMotion mouse)
+        {
+            float sensitivity = 0.01f;
+
+            // Adjust yaw and pitch
+            _yaw += -mouse.Relative.X * sensitivity;
+            _pitch += -mouse.Relative.Y * sensitivity;
+
+            // Rebuild the quaternion from yaw and pitch
+            Quaternion yawQuat = new(Vector3.Up, _yaw);
+            Quaternion pitchQuat = new(Vector3.Right, _pitch);
+
+            _quatYawPitch = yawQuat * pitchQuat;
+        }
     }
 
     public void SwitchState(State newState)
@@ -59,8 +83,8 @@ public partial class SpaceShip : Node3D, IStateMachine
             Enter = () =>
             {
                 new GTween(this)
-                    .Animate("position", Position + Vector3.Up * 3, 2).EaseIn()
-                    .Animate("position", Position + Vector3.Up * 20, 3).EaseOut()
+                    .Animate("position", Position + Vector3.Up * 3, 1).EaseIn()
+                    //.Animate("position", Position + Vector3.Up * 20, 3).EaseOut()
                     .Callback(() => SwitchState(Flight()));
             }
         };
@@ -74,29 +98,26 @@ public partial class SpaceShip : Node3D, IStateMachine
         {
             Enter = () => 
             {
-                _body.GetNode<CollisionShape3D>().SetDeferred(nameof(CollisionShape3D
-                    .PropertyName.Disabled), false);
+                _isFlying = true;
             },
             
             Update = delta =>
             {
-                if (Input.IsActionPressed(InputActions.MoveUp))
-                {
-                    _velocity += Vector3.Forward;
-                }
-                
-                if (Input.IsActionPressed(InputActions.MoveDown))
-                {
-                    _velocity += Vector3.Back;
-                }
+                // Forward / horizontal inputs
+                Vector2 fhInputs = Input.GetVector(InputActions.MoveLeft, InputActions.MoveRight, InputActions.MoveUp, InputActions.MoveDown);
 
-                _body.Velocity = _velocity;
+                // Up / down inputs
+                float udInputs = Input.GetAxis(InputActions.SpaceshipDown, InputActions.SpaceshipUp);
+
+                _updatedPosition += Quaternion * new Vector3(fhInputs.X, udInputs, fhInputs.Y);
+
+                Position = Position.Lerp(_updatedPosition, 0.1f);
             }
         };
 
         return state;
     }
-    
+
     private void TakeControlOfShip()
     {
         _.Area3D.Get().BodyEntered += body =>
